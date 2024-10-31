4180200fad7969e6f2c6424c9620f6aae8969b0c
NinaCL2049 <ninaclangston@gmail.com>
2024-10-31 16:08:37 -0500

    afternoon update

--- index.js
+++ index.js
@@ -12,11 +12,11 @@ const PORT = process.env.PORT || 4000;
 
 // const port = 4000;
 
-// app.use(express.json)
+app.use(express.json());
 
-app.get('/', (req, res) => {
-res.json("howdy world")
-})
+// app.get('/', (req, res) => {
+// res.json("howdy world")
+// })
 
 app.get('/sessions', (req, res) => {
   pool.query("SELECT * FROM sessions", function(err, rows, fields) {
@@ -39,14 +39,34 @@ app.get('/schedules', (req, res) => {
   });
 });
 
-// app.get('/users/:id', (req, res) => {
-//   const { id } = req. params;
+app.get('/users/:id', (req, res) => {
+  const { id } = req. params;
+  pool.query(
+    `SELECT * FROM users WHERE user_id = ${id}`,
+    function (err, rows, fields) {
+      res.json(rows);
+    }
+  );
+});
+
+app.get('/sessions/:id', (req, res) => {
+  const { id } = req.params;
+  pool.query(
+    `SELECT * FROM sessions WHERE user_id = ${id}`, 
+    function(err, rows, fields) {
+    res.json(rows)
+  }
+);
+});
+
+// app.get('/sessions/:id', (req, res) => {
+//   const { id } = req.params;
 //   pool.query(
-//     `SELECT * FROM users WHERE user_id = ${id}`,
-//     function (err, rows, fields) {
-//       res.json(rows);
-//     }
-//   );
+//     `SELECT * FROM sessions WHERE user_id = ${id}`, 
+//     function(err, rows, fields) {
+//     res.json(rows)
+//   }
+// );
 // });
 
 // app.post('/users/', (req, res) => {
@@ -59,6 +79,30 @@ app.get('/schedules', (req, res) => {
 //   );
 
 // });
+
+app.post('/users', (req, res) => {
+  pool.query(`INSERT INTO users (user_id, login_id, login_pwd, first_name, last_name, email) VALUES (?, ?, ?, ?, ?, ?)`,
+    [null, req.body.login_id, req.body.login_pwd, req.body.first_name, req.body.last_name, req.body.email],
+    function (err, row, fields) {
+      res.json(row);
+    }
+
+  )
+})
+
+app.post('/sessions', (req, res) => {
+  pool.query(`INSERT INTO sessions (user_id, session_id, pain, depth, notes, finish) VALUES (?, ?, ?, ?, ?, ?)`,
+    [req.body.user_id, null, req.body.pain, req.body.depth, req.body.notes, null],
+    function (err, row, fields) {
+      res.json(row);
+    }
+
+  )
+})
+
+// app.post('/users', (req, res) => {
+//   pool.query(`INSERT INTO users (user_id, login_id, login_pwd, first_name, last_name, email) VALUES (?, ?, ?, ?, ?, ?)`,
+// })
 
 app.listen(PORT, () => console.log(`Listening @ http://localhost:${PORT}`));
 // app.listen(port, () => console.log(`Listening @ http://localhost:${port}`));
